230da5d7ff135dd0fcfe53a135485a7ff5900e07
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-20 00:24:10 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,8 +10,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  index = user_input.to_i - 1
+def input_to_index(input)
+  index = input.to_i - 1
   index
 end
 
