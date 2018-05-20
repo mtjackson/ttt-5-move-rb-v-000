b7c4cc1b0b58b6be9d93f81d8c2808464783e567
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-20 01:26:53 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,11 +7,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
+def input_to_index(input="1")
   index = input.to_i - 1
 end
 
-def move(board, index="0", value="X")
+def move(board, index, value="X")
   board[index] = value
 end
 
